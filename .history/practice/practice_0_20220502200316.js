96c8ae3a02eaa56fe7e87ab3668d2fa0f752e2f5
xzm1997 <xuzhiming1997@163.com>
2022-05-02 20:06:53 +0800

    add

--- .history/practice/practice_0_20220502200316.js
+++ .history/practice/practice_0_20220502200316.js
@@ -1,9 +1,9 @@
 const promise1 = new Promise((resolve, reject) => {
-  console.log('promise1')
+  // console.log('promise1')
   resolve('resolve1')
 })
 const promise2 = promise1.then(res => {
-  console.log(res)
+  // console.log(res)
 })
 console.log('1', promise1);
 console.log('2', promise2);
